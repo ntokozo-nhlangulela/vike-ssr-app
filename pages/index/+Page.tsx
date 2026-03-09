17eb66c77a11729c5c936236dc11991b0a235d1b
Ntokozo Nhlangulela <ntokozo.n2@contractor.curro.co.za>
2026-03-09 10:41:57 +0200

    Added dynamic routing tested by user's page

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -5,7 +5,15 @@ export default function Page() {
       <h1>Home Page</h1>
       <p>Welcome to the Vike SSR learning project.</p>
 
-      <a href="/about">Go to About Page</a>
+        <a href="/about">Go to About Page</a>
+        <br/>
+        <a href="/users">Users</a>
+        <br/>
+        <a href="/products/1">Product 1</a>
+        <br />
+        <a href="/products/2">Product 2</a>
+        <br/>
+        <a href="/docs/getting-started">Docs</a>
     </main>
   );
 }
