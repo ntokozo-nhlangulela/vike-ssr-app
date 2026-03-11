e57ce769b65fa0f96305d084575f5fec88b6a26c
Ntokozo Nhlangulela <ntokozo.n2@contractor.curro.co.za>
2026-03-11 10:25:24 +0200

    fix: :bug: removed unnecessary "/"

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <br/>
         <a href="/users">Users</a>
         <br/>
-        <a href="/products/">Product</a>
+        <a href="/products">Product</a>
         <br/>
         <a href="/docs/getting-started">Docs</a>
     </main>
